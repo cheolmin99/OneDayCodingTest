1350940098c4ed1c6a0cb8ede35fadf9379a9785
cheolmin99 <cjfals8570@naver.com>
2024-05-17 23:36:28 +0900

    신기한 수

---
+++ test17618.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class test17618 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int count = 0;
+
+        for(int i=1; i<=num; i++){
+            int sum = 0;
+            int n = i;
+
+            while(n!=0){
+                sum += n%10;
+                n=n/10;
+            }
+            if(i%sum == 0) count++;
+        }
+        System.out.println(count);
+    }
+}
