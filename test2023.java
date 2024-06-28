418c05ae0394679a7fcd78feb724b88bafc79c94
cheolmin99 <cjfals8570@naver.com>
2024-06-28 21:47:53 +0900

    유기농 배추, 암호 만들기, 신기한 소수

---
+++ test2023.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class test2023 {
+    static int n;
+    public static boolean isPrime(int n) {
+        if(n < 2) return false;
+        for(int i = 2; i < Math.ceil(Math.sqrt(n)); i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void dfs(int num, int len) {
+        if(len == n) {
+            System.out.println(num);
+            return;
+        }
+        for(int i = 1; i <= 9; i += 2) {
+            int nextNum = num * 10 + i;
+            if(isPrime(nextNum)) {
+                dfs(nextNum, len + 1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = Integer.parseInt(sc.nextLine());
+
+        int[] start = {2, 3, 5, 7};
+
+        for(int i = 0; i < 4; i++){
+            dfs(start[i],1);
+        }
+    }
+}
