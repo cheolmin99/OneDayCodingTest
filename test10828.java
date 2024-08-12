53a8e0b2242bd10aa09ec8279190936c71e33300
cheolmin99 <cjfals8570@naver.com>
2024-08-12 22:07:39 +0900

    스택

---
+++ test10828.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test10828 {
+    public static int[] stack;
+    public static int size = 0;
+    public static void push(int x) {
+        stack[size] = x;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            int res = stack[size - 1];
+            size--;
+            return res;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        return size == 0 ? 1 : 0;
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[size - 1];
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        stack = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    int value = Integer.parseInt(st.nextToken());
+                    push(value);
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append("\n");
+                    break;
+
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+
+                case "top":
+                    sb.append(top()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+    
+}
